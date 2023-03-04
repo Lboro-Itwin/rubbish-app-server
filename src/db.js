bdb83b49aea7020b872ce70373e1129ee5fbb7a7
Astrojarma <matsaunderz@gmail.com>
2023-03-04 20:37:01 +0000

    Added database select(*)

---
+++ src/db.js
@@ -0,0 +1,13 @@
+const sb = require("@supabase/supabase-js");
+
+//should probably be in process.env but I literally do not care
+const supabaseUrl = 'https://gjugvzrcpatqfewlwvvt.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqdWd2enJjcGF0cWZld2x3dnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYyNzk0NDQsImV4cCI6MTk5MTg1NTQ0NH0.ENJfEtVHtSxRNfg_AZ0YA_5htLbtmjGTTP9O4MEGMBw"
+
+const supabase = sb.createClient(supabaseUrl, supabaseKey);
+
+async function getRecords(){ 
+    return await supabase.from("coords").select();
+}
+
+(async () => console.log(await getRecords()))();
